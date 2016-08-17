05ef7ba5ad3b627a2d4f1335e7d4ec9a592d46a1
Konstantin <2447785@gmail.com>
2016-08-17 10:29:04 +0300

    Floating button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+var floating = $('main div.project-detail div.floating-block');
+var floating_top = $(floating).css('top');
+
 $(document).ready(function(){
-   $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
+    $(floating).css('top', $(floating).offset().top);
+    $(floating).css('left', $(floating).offset().left);
+    $(floating).css('position', 'fixed');
  });
+
+$(window).resize(function() {
+    $(floating).css('position', 'absolute');
+    $(floating).css('top', floating_top);
+    $(floating).css('left', '');
+
+    $(floating).css('top', $(floating).offset().top);
+    $(floating).css('left', $(floating).offset().left);
+    $(floating).css('position', 'fixed');
+
+})
