e62d8f8698cf27fc74d61be7df5e5a37779177d9
Konstantin <2447785@gmail.com>
2016-08-18 10:32:49 +0300

    Message block

---
+++ js/feedback_block.js
@@ -0,0 +1,30 @@
+function toggleMessageForm() {
+    $('div.contact div.message-block form').css('display', 'block');
+    $('div.contact div.message-block div.message-success').css('display', 'none');
+    $('div.contact div.info').animate({
+        height: 'toggle'
+    }, {
+      queue:false,
+    });
+    $('div.contact div.message-block').animate({
+        height: 'toggle'
+    }, {
+      queue:false,
+    });
+}
+
+
+$('div.contact.container a.btn.open').add('div.contact.container a.btn-flat.close').click(toggleMessageForm);
+
+$('div.contact.container a.btn.send').click(function() {
+    $('div.contact div.message-block form').animate({
+        height: 'toggle'
+    });
+    $('div.contact div.message-block div.message-success').show();
+    setTimeout(function() {
+        if ($('div.contact div.message-block div.message-success').css('display') != 'none') {
+            $('div.contact div.message-block div.message-success').css('display', 'none');
+            toggleMessageForm();
+        }
+    }, 2000);
+});
